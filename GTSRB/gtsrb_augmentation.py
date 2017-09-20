6b3c852aee84fed9d18650af38fc97dfb74f990b
Max de D <m.dinechin@yahoo.com>
2017-09-20 16:24:59 +0200

    traitement de la base de données

---
+++ GTSRB/gtsrb_augmentation.py
@@ -0,0 +1,71 @@
+"""
+gtsrb_loader.py
+~~~~~~~~~~~~~~~
+
+Charge et augmente une base de données
+"""
+
+
+mode = 'grey'  # 'rgb', 'grey' ou 'clahe'
+
+
+# BIBLIOTHÈQUES EXTERNES
+# ----------------------
+
+import numpy as np
+from skimage import transform
+
+import matplotlib.pyplot as plt
+
+
+# LECTURE DES DONNÉES
+# -------------------
+
+
+images = np.load("data/train_" + mode + ".npy")
+labels = np.load("data/train_labels.npy")
+
+
+# ROTATIONS ET SYMÉTRIES
+# ----------------------
+
+auto_miroir_vertical = [11, 12, 13, 15, 17, 18, 22, 26, 30, 35]
+auto_miroir_horizontal = [1, 5, 12, 15, 17]
+auto_miroir_double = [12, 15, 17, 32]
+
+mutuel_miroir_vert = [[19,20], [33,34], [36,37], [38,39], [20,19], [34,33], [37,36], [39,38]]
+
+
+# AUGMENTATION
+# ------------
+
+
+
+def augmente(images, labels):
+	labels_num = np.array([np.argmax(i) for i in labels])
+	images_ext = np.empty((0, 40, 40), dtype = images.dtype)
+	labels_ext = np.empty((0, 43), dtype = labels.dtype)
+	for c in range(43):
+		print(c)
+		images_c = images[labels_num == c]
+		images_ext = np.append(images_ext, images_c, axis=0)
+		if c in auto_miroir_horizontal:
+			images_ext = np.append(images_ext, images_c[:, :, ::-1], axis=0)
+		if c in auto_miroir_vertical:
+			images_ext = np.append(images_ext, images_c[:, ::-1, :], axis=0)
+		if c in auto_miroir_double:
+			images_ext = np.append(images_ext, images_c[:, ::-1, ::-1], axis=0)
+		nouv_labels = np.full((len(images_ext) - len(labels_ext), 43), np.eye(43)[c])
+		labels_ext = np.append(labels_ext, nouv_labels, axis=0)
+		if [c, c+1] in mutuel_miroir_vert:
+			images_ext = np.append(images_ext, images_c[:, ::-1, :], axis=0)
+			nouv_labels = np.full((len(images_ext) - len(labels_ext), 43), np.eye(43)[c+1])
+			labels_ext = np.append(labels_ext, nouv_labels, axis=0)
+		if [c, c-1] in mutuel_miroir_vert:
+			images_ext = np.append(images_ext, images_c[:, ::-1, :], axis=0)
+			nouv_labels = np.full((len(images_ext) - len(labels_ext), 43), np.eye(43)[c-1])
+			labels_ext = np.append(labels_ext, nouv_labels, axis=0)
+	return images_ext, labels_ext
+
+
+# images_ext, labels_ext = augmente(images, labels)
---
+++ GTSRB/gtsrb_importation.py
@@ -0,0 +1,60 @@
+"""
+gtsrb_loader.py
+~~~~~~~~~~~~~~~
+
+Convertit et normalise les images organisés en 43 dossiers de catégories dans
+'Final_Training/'' en deux tableaux numpy : 'images' et 'labels'.
+
+Les images sont enregistrées en 40x40 piexls
+
+"""
+
+
+color = 'clahe'  # 'rgb', 'grey' ou 'clahe'
+
+
+# BIBLIOTHÈQUES EXTERNES
+# ----------------------
+
+import os
+import numpy as np
+import glob
+from skimage import io, color, transform, exposure
+
+
+# TRAITEMENT DES DONNÉES
+# ----------------------
+
+data_url = 'data/Training/'
+
+
+def traite_image(chemin_image):
+	# Lecture de l'image
+	image = io.imread(chemin_image)
+	# Redimensionnement en 40x40 pixels
+	image = transform.resize(image, (40, 40), mode='wrap')
+	# Ajustement local de l'exposition
+	if color == 'clahe':
+		image = exposure.equalize_adapthist(image)
+	# Conversion en nuances de gris
+	if not color == 'rgb':
+		image = color.rgb2gray(image)
+	return image
+
+
+def traite_label(chemin_image):
+	return int(chemin_image.split('/')[-2])
+
+
+def gtsrb(n):
+	images = []
+	labels = []
+	chemins_images = glob.glob(os.path.join(data_url, '*/*.ppm'))
+	np.random.shuffle(chemins_images)
+	
+	for chemin_image, _ in zip(chemins_images, range(n)):
+		images.append(traite_image(chemin_image))
+		labels.append(traite_label(chemin_image))
+	images = np.array(images)
+	labels = np.eye(43)[np.array(labels)]  # Conversion entiers -> catégories
+	return images, labels
