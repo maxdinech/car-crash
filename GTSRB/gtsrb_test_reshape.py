b0b1525e7e1f9b5d52c3dae71f695d699d1375a1
Max de D <m.dinechin@yahoo.com>
2017-09-24 15:51:59 +0200

    gestion des dossiers déjà existants

--- GTSRB/gtsrb_test_reshape.py
+++ GTSRB/gtsrb_test_reshape.py
@@ -21,7 +21,8 @@ labels = np.array(pd.read_csv('data/Final_Test/GT-final_test.csv', sep=';'))[:,7
 
 def reshape():
 	for i in range(0, 43):
-		os.makedirs(data_url + str(i).zfill(5))
+		try:
+			os.makedirs(data_url + str(i).zfill(5))
 	for i in range(len(labels)):
 		image = str(i).zfill(5) + '.ppm'
 		label = str(labels[i]).zfill(5)
