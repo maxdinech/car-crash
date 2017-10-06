f26a6a3bac561d06cf8b8a9be33b1ad0d82ac5bb
Max de D <m.dinechin@yahoo.com>
2017-10-06 16:39:52 +0200

    supression de fichier .tf

--- MNIST/tfMLP.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-
-# from tensorflow.examples.tutorials.mnist import input_data
-# mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-
-x = tf.placeholder(tf.float32, [None, 784])
