edf6b20d9e022505b9584c9b33bae5f5edeb6c9e
Max de D <m.dinechin@yahoo.com>
2017-10-06 16:37:33 +0200

    création : traitement auto. de la BDD

---
+++ GTSRB/traitements.py
@@ -0,0 +1,12 @@
+#!
+
+"""Script d'automatisation des manipulations sur GTSRB"""
+
+import gtsrb_augmentation as aug
+import gtsrb_test_reshape as test_reshape
+
+
+# On vérifie d'abord que les fichiers de GTSRB sont bien au bon endroit
+
+# Vérifier : train, test, test_labels.csv
+
