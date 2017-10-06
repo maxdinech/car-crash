b0722d2b8f4d98c81fcd3d8940e539aa95e19ab0
Max de D <m.dinechin@yahoo.com>
2017-10-06 17:16:43 +0200

    supression de la dépendance Keras

--- MNIST/mnist_loader.py
+++ MNIST/mnist_loader.py
@@ -14,7 +14,6 @@ sont des matrices de 28*28 pixels en nuances de gris (blanc = 0, noir = 1).
 
 import numpy as np
 import pandas as pd
-from keras.utils.np_utils import to_categorical
 
 mode = 'train'  # 'train' ou 'test'
 
@@ -29,7 +28,7 @@ def mnist():
     data = pd.read_csv(data_url, header=None)
     images = ((data.ix[:,1:]).values).astype('float32') / 255.
     images = images.reshape(images.shape[0], 28, 28)
-    labels = to_categorical(data.ix[:,0].values.astype('int32'), 10)
+    labels = np.eye(10)[data.ix[:,0].values.astype('int32')]
     return images, labels
 
 
