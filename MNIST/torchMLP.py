c79d04ee7cb003ba63169962685f308745d38d2e
Max de D <m.dinechin@yahoo.com>
2017-10-07 21:16:41 +0200

    paramètres de NN&DL

--- MNIST/torchMLP.py
+++ MNIST/torchMLP.py
@@ -8,9 +8,9 @@ import torch.nn.functional as F
 
 # Hyperparamètres
 # ---------------
-lr = 0.1  # taux d'aprentissage
-epochs = 1000
-batch_size = 128
+lr = 3  # taux d'aprentissage
+epochs = 30
+batch_size = 10
 
 
 # Utilise automatiquement le GPU si CUDA est disponible
@@ -30,10 +30,10 @@ images = Variable(torch.from_numpy(images[:50000]).type(dtype), requires_grad=Fa
 labels = Variable(torch.from_numpy(labels[:50000]).type(dtype), requires_grad=False)
 
 
-# On définit à la main un MLP avec une couche cachée de 128 neurones
-w1 = Variable(torch.randn(28*28, 128).type(dtype), requires_grad=True)
-b1 = Variable(torch.randn(128).type(dtype), requires_grad=True)
-w2 = Variable(torch.randn(128, 10).type(dtype), requires_grad=True)
+# On définit à la main un MLP avec une couche cachée de 30 neurones
+w1 = Variable(torch.randn(28*28, 30).type(dtype), requires_grad=True)
+b1 = Variable(torch.randn(30).type(dtype), requires_grad=True)
+w2 = Variable(torch.randn(30, 10).type(dtype), requires_grad=True)
 b2 = Variable(torch.randn(10).type(dtype), requires_grad=True)
 
 start = timeit.default_timer()
@@ -45,29 +45,30 @@ for e in range(epochs):
     ordre = torch.randperm(50000).type(ltype)
     images = images[ordre]
     labels = labels[ordre]
-
+    
     for i in range(0, 50000, batch_size):
         x = images[i:i+batch_size]
         y = labels[i:i+batch_size]
-
+        
         # Propagation de x dans le réseau. 
         a = F.relu(x @ w1 + b1)
         a = F.softmax(a @ w2 + b2)
         y_pred = a
-
+        
         # Calcul de l'erreur commise par le réseau : écart-type
-        # loss = 1/(2n) * Somme(|y - y_pred|^2 pour y dans Y)
-        loss = 5 * (y_pred - y).pow(2).mean()
-
+        # -> équivaut à F.mse_loss
+        loss = (y_pred - y).pow(2).mean()
+        # loss = F.cross_entropy(y_pred, y.max(1)[1])
+        
         # Rétropropagation du gradient sur l'erreur
         loss.backward()
-
+        
         # Ajustement des poids selon la méthode de la descente de gradient.
         w1.data -= lr * w1.grad.data
         w2.data -= lr * w2.grad.data
         b1.data -= lr * b1.grad.data
         b2.data -= lr * b2.grad.data
-
+        
         # Remise à zéro des gradients
         w1.grad.data.zero_()
         w2.grad.data.zero_()
@@ -78,9 +79,10 @@ for e in range(epochs):
     a = F.softmax(a @ w2 + b2)
     y_pred = a
 
-    val_acc = (torch.eye(10).type(dtype)[y_pred.data.max(1)[1]] * labels.data).sum() / 500
+    val_acc = (y_pred.max(1)[1] == labels.max(1)[1]).data.sum() / 500
     print(val_acc, "%")
 
 stop = timeit.default_timer()
 
 print("\n\ntemps écoulé :", stop - start)
+
