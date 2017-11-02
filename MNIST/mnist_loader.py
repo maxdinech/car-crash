4b3a6b5001264922082e3a8a9164f710d06bd53f
Max de D <m.dinechin@yahoo.com>
2017-11-02 10:44:19 +0100

    télechargement automatique de la BDD

--- MNIST/mnist_loader.py
+++ MNIST/mnist_loader.py
@@ -2,6 +2,10 @@
 
 
 import torch
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+import os
+import shutil
 
 # Utilise automatiquement le GPU si CUDA est disponible
 if torch.cuda.is_available():
@@ -9,6 +13,15 @@ if torch.cuda.is_available():
 else:
     dtype = torch.FloatTensor
 
+def créer_MNIST():
+    _ = dsets.MNIST(root='data/',
+                    train=True, 
+                    transform=transforms.ToTensor(),
+                    download=True)
+    shutil.move('data/processed/training.pt', 'data/train.pt')
+    shutil.move('data/processed/test.pt', 'data/test.pt')
+    os.remove('data/raw')
+    os.remove('data/processed')
 
 def train(nb_train, flatten=False):
     train = torch.load('data/train.pt')
@@ -23,7 +36,7 @@ def train(nb_train, flatten=False):
 
 def test(nb_val, flatten=False):
     train = torch.load('data/train.pt')
-    images, labels = train[0][:nb_train], train[1][:nb_val]
+    images, labels = train[0][:nb_val], train[1][:nb_val]
     images = images.type(dtype) / 255
     if flatten:
         images = images.view(len(images), -1)
