b45f609e5d322d2433c56ca2feff86628a0d8459
Max de D <m.dinechin@yahoo.com>
2017-11-12 20:29:00 +0100

    erreur

--- GTSRB/CNN.py
+++ GTSRB/CNN.py
@@ -137,13 +137,13 @@ for e in range(epochs):
 
     predictions_justes = 0
     for image, label in zip(train_images, train_labels):
-        if model.forward(image.view(1, 1, 40, 40)).max(1)[1] == label:
+        if model.forward(image.view(1, 1, 40, 40)).max(1)[1].data[0] == label.data[0]:
             predictions_justes += 1
     acc = predictions_justes / len(train_labels)
 
     predictions_justes = 0
     for image, label in zip(test_images, test_labels):
-        if model.forward(image.view(1, 1, 40, 40)).max(1)[1] == label:
+        if model.forward(image.view(1, 1, 40, 40)).max(1)[1].data[0] == label.data[0]:
             predictions_justes += 1
     val_acc = predictions_justes / len(test_labels)
 
