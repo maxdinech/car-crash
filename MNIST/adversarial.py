940dae3a1f77d7f1cb4615f608fb4a13e04eb7c8
Max de D <m.dinechin@yahoo.com>
2017-11-19 10:30:10 +0100

    automatisation des attaques

--- MNIST/adversarial.py
+++ MNIST/adversarial.py
@@ -9,7 +9,7 @@ import torch
 from torch.autograd import Variable
 import mnist_loader
 import matplotlib.pyplot as plt
-
+from matplotlib import rc, rcParams
 
 
 # Création de variable sur GPU si possible, CPU sinon
@@ -29,18 +29,24 @@ except FileNotFoundError:
     print("Pas de modèle trouvé !")
 
 
-def compare(image1, r, image2):
+def compare(image1, r, image2, num, p, norme):
+    rc('text', usetex=True)
+    rcParams['axes.titlepad'] = 10
     fig = plt.figure()
     ax1 = fig.add_subplot(1,3,1)
     ax1.imshow(image1.data.view(28, 28).numpy(), cmap='gray')
-    plt.title("Prédiction : {}".format(prediction(image1)))
+    plt.title("$\\textrm{{Prediction : }} {}$".format(prediction(image1)))
+    plt.axis('off')
     ax2 = fig.add_subplot(1,3,2)
     ax2.imshow(r.data.view(28, 28).numpy(), cmap='RdBu')
-    plt.title("Perturbation")
+    plt.title("$\\textrm{{Perturbation : }} \\Vert r \\Vert_{{{}}} = {}$".format(p, round(norme, 3)))
+    plt.axis('off')
     ax3 = fig.add_subplot(1,3,3)
     ax3.imshow(image2.data.view(28, 28).numpy(), cmap='gray')
-    plt.title("Prédiction : {}".format(prediction(image2)))
-    plt.show()
+    plt.title("$\\textrm{{Prediction : }} {}$".format(prediction(image2)))
+    plt.axis('off')
+    plt.subplots_adjust(left=0.05, right=0.95, top=0.95, bottom=0.05)
+    plt.savefig("../docs/images/adv/adv_{}_n{}.png".format(num, p), bbox_inches='tight')
 
 def affiche(image):
     plt.imshow(image.clamp(0, 1).data.view(28, 28).numpy(), cmap='gray')
@@ -57,20 +63,21 @@ def affiche(image):
 # Jusqu'à obtenir une prédiction incorrecte
 
 
-def charge_image(n):
-    images, _ = mnist_loader.train(n+1)
-    return to_Var(images[n].view(1, 1, 28, 28))
+def charge_image(num):
+    images, _ = mnist_loader.train(num+1)
+    return to_Var(images[num].view(1, 1, 28, 28))
 
 
 def prediction(image):
     return model.forward(image.clamp(0, 1)).max(1)[1].data[0]
 
 
-def attaque_1(n, lr=0.005, div=-0.2):
-    image = charge_image(n)
+def attaque(num, lr=0.005, div=0.2, p=2):
+    image = charge_image(num)
     chiffre = prediction(image)
     r = to_Var(torch.zeros(1, 1, 28, 28), requires_grad=True)
-    image_adv = (image + r.clamp(-div, div)).clamp(0, 1)
+    adv = lambda image, r: (image + (r * div / (1e-5 + r.norm(p)))).clamp(0, 1)
+    image_adv = adv(image, r)
     i = 0
     while prediction(image_adv) == chiffre:
         loss = model.forward(image_adv)[0,chiffre]
@@ -78,47 +85,28 @@ def attaque_1(n, lr=0.005, div=-0.2):
         print(str(i).zfill(3), loss.data[0], end='\r')
         r.data -= lr * r.grad.data / r.grad.data.abs().max()
         r.grad.data.zero_()
-        image_adv = (image + r.clamp(-div, div)).clamp(0, 1)
+        image_adv = adv(image, r)
         i += 1
-        if i >= 300:
+        if i >= 500:
             break
-    return (i < 300), image, r, image_adv
+    return (i < 500), image, (image_adv-image), image_adv
 
 
-def attaque_2(n, lr=0.005, div=0.2):
-    image = charge_image(n)
-    chiffre = prediction(image)
-    r = to_Var(torch.zeros(1, 1, 28, 28), requires_grad=True)
-    image_adv = (image + (r * div / (1e-5 + r.norm()))).clamp(0, 1)
-    i = 0
-    while prediction(image_adv) == chiffre:
-        loss = model.forward(image_adv)[0,chiffre]
-        loss.backward()
-        print(str(i).zfill(3), loss.data[0], end='\r')
-        r.data -= lr * r.grad.data / r.grad.data.abs().max()
-        r.grad.data.zero_()
-        image_adv = (image + (r * div / (1e-5 + r.norm()))).clamp(0, 1)
-        i += 1
-        if i >= 300:
-            break
-    return (i < 300), image, r, image_adv
-
-
-def attaque_optimale(n, a=0, b=5, lr=0.005):
-    if b-a < 0.01:
+def attaque_optimale(num, a=0, b=5, p=2, lr=0.005):
+    if b-a < 0.001:
         print("\n\nValeur minimale approchée : ", b)
-        succes, image, r, image_adv = attaque_2(n, lr, b)
-        compare(image, r, image_adv)
+        succes, image, r, image_adv = attaque(num, lr, b, p)
+        compare(image, r, image_adv, num, p, b)
     else:
         c = (a+b)/2
         print("\n\n", c, "\n")
-        succes, _, _, _ = attaque_2(n, lr, c)
+        succes, _, _, _ = attaque(num, lr, c, p)
         if succes:
-            attaque_optimale(n, a, c, lr)
+            attaque_optimale(num, a, c, p, lr)
         else:
-            attaque_optimale(n, c, b, lr)
+            attaque_optimale(num, c, b, p, lr)
 
 
-def attaques(lr=0.005):
-    for n in range(1000):
-        attaque(n, lr)
+def attaques(num):
+    for p in [2, 3, 5, 10]:
+        attaque_optimale(num, 0, 5, p)
