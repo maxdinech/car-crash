f953ce10175be11260cb6a7e017517933beb4df6
Max de D <m.dinechin@yahoo.com>
2017-11-22 22:29:15 +0100

    ajout de la base de validation

--- MNIST/mnist_loader.py
+++ MNIST/mnist_loader.py
@@ -25,33 +25,44 @@ def create_MNIST():
                     train=True, 
                     transform=transforms.ToTensor(),
                     download=True)
-    shutil.move('data/processed/training.pt', 'data/train.pt')
-    shutil.move('data/processed/test.pt', 'data/test.pt')
+    # On appelle `val` les images de `test`
+    shutil.move('data/processed/test.pt', 'data/val.pt')
+    # On divise `training` en `train` et `test`
+    images, labels = torch.load('data/processed/training.pt')
+    train_images, train_labels = images[:50000], labels[:50000]
+    val_images, val_labels = images[50000:], labels[50000:]
+    torch.save((train_images, train_labels), "data/train.pt")
+    torch.save((val_images, val_labels), "data/test.pt")
+    # On supprimme les dossiers temporaires
     shutil.rmtree('data/raw')
     shutil.rmtree('data/processed')
 
 
-def train(nb_train=60000, flatten=False):
+def train(nb_train=50000):
     if not os.path.exists('data/train.pt'):
         create_MNIST()
     images, labels = torch.load('data/train.pt')
     images, labels = images[:nb_train], labels[:nb_train]
     images = images.type(dtype) / 255
-    if flatten:
-        images = images.view(len(images), -1)
-    else:
-        images = images.view(len(images), 1, 28, 28)
+    images = images.view(len(images), 1, 28, 28)
     return images, labels
 
 
-def test(nb_val=10000, flatten=False):
+def test(nb_test=10000):
     if not os.path.exists('data/test.pt'):
         create_MNIST()
     images, labels = torch.load('data/test.pt')
+    images, labels = images[:nb_test], labels[:nb_test]
+    images = images.type(dtype) / 255
+    images = images.view(len(images), 1, 28, 28)
+    return images, labels
+
+
+def val(nb_val=10000):
+    if not os.path.exists('data/val.pt'):
+        create_MNIST()
+    images, labels = torch.load('data/val.pt')
     images, labels = images[:nb_val], labels[:nb_val]
     images = images.type(dtype) / 255
-    if flatten:
-        images = images.view(len(images), -1)
-    else:
-        images = images.view(len(images), 1, 28, 28)
+    images = images.view(len(images), 1, 28, 28)
     return images, labels
