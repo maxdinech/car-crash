881488795f7d8a5f74ffbe15b70b1854792fdbc4
Max de D <m.dinechin@yahoo.com>
2017-11-22 23:08:10 +0100

    ajout du dropout dans le CNN

--- MNIST/architectures.py
+++ MNIST/architectures.py
@@ -96,3 +96,35 @@ class CNN(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.softmax(self.fc2(x))
         return x
+
+
+# CNN à deux convolutions (avec dropout)
+class CNN_d(nn.Module):
+    def __init__(self):
+        super(CNN_d, self).__init__()
+        # Hyperparamètres d'entrainement
+        self.lr = 3e-4
+        self.epochs = 30
+        self.batch_size = 32
+        # Définition des couches du modèle
+        self.conv1 = nn.Conv2d(1, 20, 5)
+        self.pool1 = nn.MaxPool2d(2)
+        self.conv2 = nn.Conv2d(20, 40, 3)
+        self.pool2 = nn.MaxPool2d(2)
+        self.fc1 = nn.Linear(5*5*40, 120)
+        self.drop1 = nn.Dropout(p=0.2)
+        self.fc2 = nn.Linear(120, 10)
+        self.drop2 = nn.Dropout(p=0.2)
+        # fonctions d'erreur et optimiseur
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
+        self.loss_fn = nn.CrossEntropyLoss()
+
+    def forward(self, x):
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+        x = x.view(len(x), -1)  # Flatten
+        x = self.drop1(x)
+        x = F.relu(self.fc1(x))
+        x = self.drop2(x)
+        x = F.softmax(self.fc2(x))
+        return x
