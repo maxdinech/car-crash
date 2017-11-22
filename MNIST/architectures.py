ee0bf8ca392e7cdd0abbe215313940aeb6892022
Max de D <m.dinechin@yahoo.com>
2017-11-22 22:27:50 +0100

    ajout du MLP et description

--- MNIST/architectures.py
+++ MNIST/architectures.py
@@ -2,6 +2,14 @@
 Architectures de réseaux PyTorch utilisés pour la reconaissance de MNIST.
 Ce fichier permet de partager les architectures utilisées avec adversarial.py
 
+Chaque réseau contient ses informations d'apprentissage :
+    - lr, epochs et batch_size
+    - loss_fn
+    - optimizer
+
+Ce qui permet de ne pas avoir à changer tous les HP à chaque fois que l'on
+change de réseau.
+
 """
 
 
@@ -10,17 +18,50 @@ from torch import nn
 import torch.nn.functional as F
 
 
-# Définition du modèle : CNN à deux convolutions
+
+
+# MLP à deux couches cachées (28*28 -> 128 -> 128 -> 10)
+class MLP(nn.Module):
+    def __init__(self):
+        super(MLP, self).__init__()
+        # Hyperparamètres d'entrainement
+        self.lr = 3e-4
+        self.epochs = 30
+        self.batch_size = 32
+        # Définition des couches du modèle
+        self.fc1 = nn.Linear(28*28, 120)
+        self.fc2 = nn.Linear(120, 120)
+        self.fc3 = nn.Linear(120, 10)
+        # fonctions d'erreur et optimiseur
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
+        self.loss_fn = nn.CrossEntropyLoss()
+
+    def forward(self, x):
+        x = x.view(len(x), -1)  # Flatten
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.softmax(self.fc3(x))
+        return x
+
+
+# CNN à deux convolutions
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
-        # convolutions : nb_canaux_entree, nb_canaux_sortie, dim_kernel
+        # Hyperparamètres d'entrainement
+        self.lr = 3e-4
+        self.epochs = 30
+        self.batch_size = 32
+        # Définition des couches du modèle
         self.conv1 = nn.Conv2d(1, 20, 5)
         self.pool1 = nn.MaxPool2d(2)
         self.conv2 = nn.Conv2d(20, 40, 3)
         self.pool2 = nn.MaxPool2d(2)
         self.fc1 = nn.Linear(5*5*40, 120)
         self.fc2 = nn.Linear(120, 10)
+        # fonctions d'erreur et optimiseur
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
+        self.loss_fn = nn.CrossEntropyLoss()
 
     def forward(self, x):
         x = self.pool1(F.relu(self.conv1(x)))
